f96620c8e04765a9870a02203147f3782f97e77f
foxsexy198 <lankc3939@gmail.com>
2023-10-28 14:09:51 +0700

    Hien thi danh sach cac contact lay tu server

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,37 @@
+<script>
+    export default {
+        props: {
+            contact: {type: Object, required: true},
+        },
+    };
+</script>
+
+<template>
+    <div>
+        <div class="p-l">
+            <strong>Tên:</strong>
+            {{ contact.name }}
+        </div>
+
+        <div class="p-l">
+            <strong>E-mail:</strong>
+            {{ contact.email }}
+        </div>
+
+        <div class="p-l">
+            <strong>Địa chỉ:</strong>
+            {{ contact.address }}
+        </div>
+
+        <div class="p-l">
+            <strong>Điện thoại:</strong>
+            {{ contact.phone }}
+        </div>
+
+        <div class="p-l">
+            <strong>Liên hệ yêu thích:&nbsp;</strong>
+            <i class="fas fa-check" v-if="contact.favorite"></i>
+            <i class="fas fa-times" v-else></i>
+        </div>
+    </div>
+</template>
